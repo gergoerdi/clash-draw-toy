c02644c98ce34ee0111ac351dcff64c9f6e1b926
Dr. ERDI Gergo <gergo@erdi.hu>
2020-05-02 19:50:56 +0800

    A Clash drawing toy

---
+++ Setup.hs
@@ -0,0 +1,16 @@
+import Clash.Clashilator.Setup (clashilate)
+
+import Distribution.Simple
+import Distribution.Simple.LocalBuildInfo
+import Distribution.Simple.Setup
+import Distribution.PackageDescription
+
+main :: IO ()
+main = defaultMainWithHooks simpleUserHooks
+    { buildHook = myBuildHook
+    }
+
+myBuildHook :: PackageDescription -> LocalBuildInfo -> UserHooks -> BuildFlags -> IO ()
+myBuildHook pkg localInfo userHooks buildFlags = do
+    pkg <- clashilate pkg localInfo buildFlags "DrawToy"
+    buildHook simpleUserHooks pkg localInfo userHooks buildFlags
