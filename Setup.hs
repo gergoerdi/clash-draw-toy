9175ea5a3c912d0fb5d5934234c8d5f26d95dc13
Dr. ERDI Gergo <gergo@erdi.hu>
2020-05-05 13:33:55 +0800

    Only Clashilate if `verilator` flag is turned on

--- Setup.hs
+++ Setup.hs
@@ -5,6 +5,8 @@ import Distribution.Simple.LocalBuildInfo
 import Distribution.Simple.Setup
 import Distribution.PackageDescription
 
+import Data.Maybe
+
 main :: IO ()
 main = defaultMainWithHooks simpleUserHooks
     { buildHook = myBuildHook
@@ -12,5 +14,8 @@ main = defaultMainWithHooks simpleUserHooks
 
 myBuildHook :: PackageDescription -> LocalBuildInfo -> UserHooks -> BuildFlags -> IO ()
 myBuildHook pkg localInfo userHooks buildFlags = do
-    pkg <- clashilate pkg localInfo buildFlags "DrawToy"
+    let flags = configConfigurationsFlags . configFlags $ localInfo
+        verilatorFlag = fromMaybe False $ lookupFlagAssignment (mkFlagName "verilator") flags
+
+    pkg <- if verilatorFlag then clashilate pkg localInfo buildFlags "DrawToy" else return pkg
     buildHook simpleUserHooks pkg localInfo userHooks buildFlags
