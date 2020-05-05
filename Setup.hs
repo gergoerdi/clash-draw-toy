a4d9dd006b38307230e5be291a211820ef1f6783
Dr. ERDI Gergo <gergo@erdi.hu>
2020-05-05 21:07:43 +0800

    Update Clashilator

--- Setup.hs
+++ Setup.hs
@@ -1,21 +1,4 @@
-import Clash.Clashilator.Setup (clashilate)
-
-import Distribution.Simple
-import Distribution.Simple.LocalBuildInfo
-import Distribution.Simple.Setup
-import Distribution.PackageDescription
-
-import Data.Maybe
+import Clash.Clashilator.Setup (clashilatorMain)
 
 main :: IO ()
-main = defaultMainWithHooks simpleUserHooks
-    { buildHook = myBuildHook
-    }
-
-myBuildHook :: PackageDescription -> LocalBuildInfo -> UserHooks -> BuildFlags -> IO ()
-myBuildHook pkg localInfo userHooks buildFlags = do
-    let flags = configConfigurationsFlags . configFlags $ localInfo
-        verilatorFlag = fromMaybe False $ lookupFlagAssignment (mkFlagName "verilator") flags
-
-    pkg <- if verilatorFlag then clashilate pkg localInfo buildFlags "DrawToy" else return pkg
-    buildHook simpleUserHooks pkg localInfo userHooks buildFlags
+main = clashilatorMain
